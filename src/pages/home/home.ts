47b813afd7c4e95eba525284e728989c36506a9c
univgopal07 <univgopal07@gmail.com>
2017-04-16 22:31:47 -0500

    changed app color, removed tabs and added home page as only tab

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
-import { Component, NgZone  } from '@angular/core';
+import { Component, NgZone, ViewChild  } from '@angular/core';
 import { Platform, ToastController  } from 'ionic-angular';
-import { NavController } from 'ionic-angular';
+import { NavController, Content } from 'ionic-angular';
 import { TextToSpeech } from '@ionic-native/text-to-speech';
 import { Http, Headers, RequestOptions } from '@angular/http';
 
@@ -24,6 +24,9 @@ export class HomePage {
   public isSpeaking : boolean = false;
   public spokenWords : Array<string> = new Array<string>();
   public messages: any[];
+  public isLoggedIn : boolean = false;
+  public texting : string;
+  @ViewChild(Content) content: Content;
 
   counter : number = 0;
 
@@ -36,71 +39,93 @@ export class HomePage {
 
     platform.ready().then(() => {
       this.ready = true;
-      console.log("Recognition is " + this.recognition)
-      if(window.SpeechRecognition)
+      if(this.isLoggedIn){
+        this.AppStart();
+      }
+      else
       {
-          this.recognition = new window.SpeechRecognition();
-          this.recognition.start();
+        this.tts.speak("Please say secret phrase").then(() => {
+            this.AppStart();
+          }
+        ).catch();
+      }
+    });
+  }
 
-          this.recognition.continuous = true;
-          this.recognition.lang = 'en-US';
-          this.recognition.maxAlternatives = 3;
-          this.recognition.onnomatch = (event => {
-              console.log('No match found.');
-          });
-          this.recognition.onstart = (event => {
-              console.log('Started recognition.');
-              this._zone.run(() => {
-                  this.isRecognizing = true;
-                  this.isWaiting = false;
-              }
-              )
-          });
+  AppStart(){
+    if(window.SpeechRecognition)
+    {
+        this.recognition = new window.SpeechRecognition();
+        this.recognition.continuous = true;
+        this.recognition.lang = 'en-IN';
+        this.recognition.maxAlternatives = 3;
+        this.recognition.onnomatch = (event => {
+            console.log('No match found.');
+        });
+        this.recognition.onstart = (event => {
+            console.log('Started recognition.');
+            this._zone.run(() => {
+                this.isRecognizing = true;
+                this.isWaiting = false;
+            }
+            )
+        });
 
-          this.recognition.onend = (event => {
-              console.log('Stopped recognition.');
-              this._zone.run(() => {
-                  this.isRecognizing = false;
-                  this.presentToast("Stopped recognition.")
-                  this.isWaiting = false;
-              })
-          });
+        this.recognition.onend = (event => {
+            console.log('Stopped recognition.');
+            this._zone.run(() => {
+                this.isRecognizing = false;
+                this.presentToast("Stopped recognition.")
+                this.isWaiting = false;
+            })
+        });
 
-          this.recognition.onerror = (event => {
-              console.log('Error...' + event.error);
-              this._zone.run(() => {
-                  this.errorCordova = 'Error'
-                  this.isRecognizing = false;
-                  this.presentToast("Error..." + event.error)
-                  this.isWaiting = false;
-              }
-              )
-          });
+        this.recognition.onerror = (event => {
+            console.log('Error...' + event.error);
+            this._zone.run(() => {
+                this.errorCordova = 'Error'
+                this.isRecognizing = false;
+                this.presentToast("Error..." + event.error)
+                this.isWaiting = false;
+            }
+            )
+        });
 
-          this.recognition.onresult = (event => {
-                  if (event.results) {
-                        this._zone.run(() => {
-                            var result = event.results[0];
+        this.recognition.onresult = (event => {
+                if (event.results) {
+                      this._zone.run(() => {
+                          var result = event.results[0];
+                          if(this.isLoggedIn){
                             this.resultText = 'Last Text added : \n' + result[0].transcript;
                             this.spokenWords.push(result[0].transcript);
                             this.AppendText("User",result[0].transcript);
                             this.SendToAPIAI(result[0].transcript);
                             console.log('Text: ' + result[0].transcript);
                             this.presentToast(this.resultText);
-                    }
-                )
-                this.isWaiting = false;
-                        this.isRecognizing = false;
-              }
-          });
-
-      }
-    });
+                          }
+                          else{
+                            if(result[0].transcript==="morning"){
+                              this.isLoggedIn = true;
+                              this.ToSpeech("Welcome to I C I C I Voice");
+                              this.SendToAPIAI("fetch alert");
+                            }
+                            else{
+                              this.ToSpeech("You have not yet authenticated, please tell me your secret phrase");
+                            }
+                          }
+                  }
+              )
+              this.isWaiting = false;
+                      this.isRecognizing = false;
+            }
+        });
+    }
   }
 
   AppendText(username, textresult){
     var result = {user: username, text: textresult};
     this.messages.push(result);
+    this.scrollToBottom();
   }
   presentToast(message) {
     let toast = this.toastCtrl.create({
@@ -114,6 +139,7 @@ export class HomePage {
 
   SpeechToText(){
     this.statusSpeaker = 'Waiting...';
+
     if(!this.isRecognizing)
     {
         this.recognition.start();
@@ -123,6 +149,8 @@ export class HomePage {
     {
         this.isWaiting = true;
     }
+
+
   }
   ToSpeech(texttotalk){
     try{
@@ -133,6 +161,7 @@ export class HomePage {
     {
       this.presentToast(e);
     }
+
 
     }
 
@@ -156,7 +185,11 @@ export class HomePage {
     }
     Send(textedquery){
       this.AppendText("User",textedquery);
+      this.texting = "";
       this.SendToAPIAI(textedquery);
+    }
+    scrollToBottom(){
+      this.content.scrollToBottom();
     }
 
 
